08964a9732088584b8e79c9a5f4c2c4ba8fecd58
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-01-11 08:30:16 -0800

    Done with Wk/Ex01

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello my World!!!! I'm Elliot.");
+        Console.Write("Hello my World!!!! I'm Elliot.");
+        Console.WriteLine("I am back again to run a practical");
+
+        Console.Write("you know my name so what is your name? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"well how are you doing {name}.");
     }
 }
--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,8 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    // this is wk01
+    // this is wk01 Assigmet 
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
