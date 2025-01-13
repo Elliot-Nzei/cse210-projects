c002024c4fc50fe43acd7eaffdc11457f06f9966
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-01-13 11:36:15 -0800

    Sorry for the late submittion

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,12 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hello my World!!!! I'm Elliot.");
-        Console.WriteLine("I am back again to run a practical");
+        int x = 12;
+        int y = 12; 
 
-        Console.Write("you know my name so what is your name? ");
-        string name = Console.ReadLine();
-
-        Console.WriteLine($"well how are you doing {name}.");
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else if (x == y)
+        {
+            Console.WriteLine("Equal");
+        }
+        else 
+        {
+            Console.WriteLine("Equl");
+        }
     }
 }
