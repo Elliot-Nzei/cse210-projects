9eb4c501edc52196d958cae9eaf793960f325545
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-01-14 04:37:53 -0800

    Sorry for the late submittion, i had some problems with my laptop

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0) // if input is not 0 add input 
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0); // whil input is 0 End Loop
+
+
+            //seperate get sin
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+
+            //seperate hard
+        double average = (double)sum / numbers.Count;
+
+        int max = numbers[0]; 
+
+
+            //seperate get Max
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        // Display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
