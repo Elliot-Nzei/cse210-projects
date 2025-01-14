da8a52a6a6d49c564d5d42a1bfc183d8ef41e6d2
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-01-14 09:58:59 -0800

    Very confusing

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    
+    static void DisplayWelcome() 
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
+
+    
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+    }
+
+    static void Main(string[] args) 
+    {
+    
+        DisplayWelcome(); 
+        
+        string name = PromptUserName(); 
+        int number = PromptUserNumber(); 
+        
+        int squared = SquareNumber(number); 
+        
+        DisplayResult(name, squared); 
     }
 }
