641d38897815b6da496e47dceca56a500c87d58c
Elliot Ekene Nzei <elliotnzei41@gmail.com>
2025-02-19 19:32:03 -0800

    done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Square("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
